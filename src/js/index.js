29470302df4ca16ecdb8c0885e6eaa616ed58a1b
Ramil Novruzov <ramil.novruzov@adesso.de>
2023-08-24 15:52:43 +0200

    backdrop + refactor + cleaning

--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,15 @@ const mobileNav = document.querySelector('.mobile-nav')
 const burgerIcon = document.querySelector('.burger-icon')
 const closeIcon = document.querySelector('.close-icon')
 
+const overlay = document.querySelector('.overlay')
 burger.addEventListener('click', function(){
   mobileNav.classList.toggle('d-flex')
   burgerIcon.classList.toggle('d-none')
   closeIcon.classList.toggle('d-block')
+  overlay.classList.toggle('d-block')
+  burger.classList.toggle('p-l-30')
 })
+
+
+
+

4b4d7eb944c97df2052b0ba9f11a94a7d88da6a5
Ramil Novruzov <ramil.novruzov@adesso.de>
2023-08-31 12:45:55 +0200

    backdrop fixed

--- src/js/index.js
+++ src/js/index.js
@@ -4,14 +4,17 @@ const mobileNav = document.querySelector('.mobile-nav')
 const burgerIcon = document.querySelector('.burger-icon')
 const closeIcon = document.querySelector('.close-icon')
 
-const overlay = document.querySelector('.overlay')
-burger.addEventListener('click', function(){
+const backdrop = document.querySelector('.backdrop')
+
+burger.addEventListener('click', function () {
   mobileNav.classList.toggle('d-flex')
   burgerIcon.classList.toggle('d-none')
   closeIcon.classList.toggle('d-block')
-  overlay.classList.toggle('d-block')
+  backdrop.classList.toggle('d-block')
   burger.classList.toggle('p-l-30')
 })
+
+
 
 
 
